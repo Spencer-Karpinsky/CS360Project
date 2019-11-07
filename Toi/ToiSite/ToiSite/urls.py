6e7966a92108bc5660559c1eba72653c7b9772d8
Alex <alexgreer04@gmail.com>
2019-11-06 19:29:52 -0600

    added the django debug toolbar for performance testing

--- Toi/ToiSite/ToiSite/urls.py
+++ Toi/ToiSite/ToiSite/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 from ToiApp import views
 from accounts import views as account_views
-
+import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
@@ -31,17 +31,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='Login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('ToiApp.urls')),
-
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-
-    ] + urlpatterns
-    
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
