857a313a47979376e7f2942aa7f347a26d97b4ca
spencer-karpinsky <spencer.karpinsky@gmail.com>
2019-11-22 11:20:27 -0600

    docker set up

--- Toi/ToiSite/ToiSite/settings.py
+++ Toi/ToiSite/ToiSite/settings.py
@@ -99,10 +99,18 @@ WSGI_APPLICATION = 'ToiSite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'postgres',
+        'PORT': '5432'
+        }
 }
 
 
